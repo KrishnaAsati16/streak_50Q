41d9263a7043ba2d349a4b5e47ffa55a27252872
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-04 10:46:44 +0530

    Find all pairs with a given sum

--- array14.java
+++ array14.java
@@ -1,18 +1,48 @@
+import java.util.Arrays;
+
 public class array14 {
     // Approach 1
-    public static void main(String[] args) {
-        int arr[] = {4,8,10,2,5,7,5,2,6,6,12,0};
-        int target = 12;
-        System.out.println("Pairs = " + target + ":");
+    // public static void main(String[] args) {
+    //     int arr[] = {4,8,10,2,5,7,5,2,6,6,12,0};
+    //     int target = 12;
+    //     System.out.println("Pairs = " + target + ":");
           
-        for(int i=0;i<arr.length;i++){
-          for(int j=0;j<arr.length;j++){
-            if(arr[i]+arr[j]==target) 
-                System.out.println(arr[i] + ", " + arr[j]);
+    //     for(int i=0;i<arr.length;i++){
+    //       for(int j=0;j<arr.length;j++){
+    //         if(arr[i]+arr[j]==target) 
+    //             System.out.println(arr[i] + ", " + arr[j]);
                 
-            }
+    //         }
 
-          }
+    //       }
+              // Approach 2
+
+           //   TWO POINTER 
+              public static void main(String[] args) {
+        int[] arr = {4,8,10,2,5,7,5,2,6,6,12,0};
+        int target = 12;
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        System.out.println("Pairs = " + target + ":");
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println(arr[left] + ", " + arr[right]);
+                left++;
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
         }
-    }
+              }
+            }
+    
     
